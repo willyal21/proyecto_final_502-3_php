eb42b09a5b3a78a306ef35f125a0914637d9499e
Adrian Abarca <Adrian Abarca@WilliamAbarcaL>
2018-09-28 08:12:47 -0600

    280920180811

---
+++ dist/js/general.js
@@ -0,0 +1,13 @@
+function login(){
+
+	name = document.getElementById("nombre").value;
+	pass = document.getElementById("password").value;
+//	
+//	if(name != ""){
+		
+//		sessionStorage.setItem("usuario", name);
+		window.location = "pages/ventas/catalogo.php";
+		
+//	}
+	
+}
